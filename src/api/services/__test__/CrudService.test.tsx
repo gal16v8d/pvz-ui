8172300aaa69180fcf89054f5b27b8c84fec5ce0
alexg <alex.galvis.sistemas@gmail.com>
2023-05-06 10:40:05 -0500

    Enable MSW for Vite Project

--- src/api/services/__test__/CrudService.test.tsx
+++ src/api/services/__test__/CrudService.test.tsx
@@ -2,15 +2,12 @@ import { get } from '../CrudService';
 import { API_BASE_CONFIG, API_OBJECT } from '@/config/ApiBaseConfig';
 
 describe('Crud service tests', () => {
-  // temp skip per MSW + VIte config errors
-  it.skip('get success', async () => {
+  it('get call return success response', async () => {
     const minigameConfig = API_BASE_CONFIG.filter(
       (v) => v.name === API_OBJECT.MINIGAME
     )?.[0];
-    const result = await get(
-      `http://localhost:7000${minigameConfig.route}`,
-      false
-    );
-    expect(result).toHaveLength(10);
+    const route = `http://localhost:7000${minigameConfig.route}`;
+    const result = await get(route, false);
+    expect(result).toHaveLength(20);
   });
 });
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,14 @@
-import 'whatwg-fetch';
+import fetch, { Headers, Request, Response } from 'node-fetch';
 import { server } from './mocks/server';
+
+// @ts-ignore
+globalThis.fetch = fetch;
+// @ts-ignore
+globalThis.Headers = Headers;
+// @ts-ignore
+globalThis.Request = Request;
+// @ts-ignore
+globalThis.Response = Response;
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
