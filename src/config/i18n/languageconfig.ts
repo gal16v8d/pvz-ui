6a9042d5b2e4dbf7c8fab751b2e0c7d2339a377d
A. Galvis <alex.galvis.sistemas@gmail.com>
2026-03-01 17:15:13 -0500

    Add some lang support

--- src/config/i18n/languageconfig.ts
+++ src/config/i18n/languageconfig.ts
@@ -3,10 +3,14 @@ import languageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 import english from './en/translation.json';
 import spanish from './es/translation.json';
+import chinese from './zh/translation.json';
+import japanese from './ja/translation.json';
 
 const resources = {
   en: { translation: english },
   es: { translation: spanish },
+  zh: { translation: chinese },
+  ja: { translation: japanese },
 };
 
 void i18next
